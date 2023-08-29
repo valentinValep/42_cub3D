23840b455d84f06a9820f5b99f10573fd498f320
Valentin LEPILLER <vlepille@student.42.fr>
2023-08-29 16:20:03 +0200

    Render player on minimap

--- sources/rendering.c
+++ sources/rendering.c
@@ -1,6 +1,24 @@
 #include "cub3d.h"
 
 #define MAP_SCALE 10
+
+void	render_player(t_context *context)
+{
+	const int	x = context->map.player.x * MAP_SCALE
+		+ WIN_WIDTH - context->map.width * MAP_SCALE;
+	const int	y = context->map.player.y * MAP_SCALE;
+
+	set_img_pixel(&context->img, x, y, 0x0000FF);
+	set_img_pixel(&context->img, x - 1, y - 1, 0x0000FF);
+	set_img_pixel(&context->img, x - 1, y, 0x0000FF);
+	set_img_pixel(&context->img, x - 1, y + 1, 0x0000FF);
+	set_img_pixel(&context->img, x, y - 1, 0x0000FF);
+	set_img_pixel(&context->img, x, y + 1, 0x0000FF);
+	set_img_pixel(&context->img, x + 1, y - 1, 0x0000FF);
+	set_img_pixel(&context->img, x + 1, y, 0x0000FF);
+	set_img_pixel(&context->img, x + 1, y + 1, 0x0000FF);
+}
+
 void	render_minimap(t_context *context)
 {
 	char	c;
@@ -12,9 +30,10 @@ void	render_minimap(t_context *context)
 				for (int l = 0; l < MAP_SCALE; l++)
 				{
 					c = get_map_char(&context->map, x, y);
-					set_img_pixel(&context->img, x * MAP_SCALE + k + WIN_WIDTH - context->map.width * MAP_SCALE, y * MAP_SCALE + l, c == '1' ? 0xFF0000 : c == '0' ? 0xFFFFFF : 0x0000FF);
+					set_img_pixel(&context->img, x * MAP_SCALE + k + WIN_WIDTH - context->map.width * MAP_SCALE, y * MAP_SCALE + l, c == '1' ? 0xFF0000 : 0xFFFFFF);
 				}
 		}
+	render_player(context);
 }
 
 void	render_raycasting(t_context *context)
