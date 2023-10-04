f8141236220c0730416e875e978bb14893b9c563
Fabrice GUARRACI <fguarrac@student.42.fr>
2023-10-04 20:17:22 +0200

    Add contrast to minimap walls. Add minimap dezoom when running

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -92,11 +92,13 @@ typedef struct s_inputs_handler {
 	char	active_collisions;
 }	t_inputs_handler;
 
-typedef struct	s_minimap_rays
+typedef struct	s_minimap
 {
-	t_ray			ray;
-	t_nearest_wall	wall;
-}				t_minimap_rays;
+//	t_ray			ray;
+//	t_nearest_wall	wall;
+	int				max_search;
+	int				mm_zoom;
+}				t_minimap;
 
 typedef struct s_context {
 	void				*mlx;
@@ -106,7 +108,7 @@ typedef struct s_context {
 	int					win_height;
 	t_map				map;
 	t_inputs_handler	inputs_handler;
-	t_minimap_rays		minimap[3];	//	To store minimap data. Need to remove magic number!
+	t_minimap			minimap;	//	To store minimap data. Need to remove magic number!
 }	t_context;
 
 // errors.c
--- sources/rendering_minimap.c
+++ sources/rendering_minimap.c
@@ -7,26 +7,38 @@
 
 static void	draw_square(t_context *context, int x, int y)
 {
-	for (int i = 0; i < MM_ZOOM * WIN_SIZE_PROPORTION/*running ? 3 : 5*/; i++)	//	Magic number
-		for (int j = 0; j < MM_ZOOM * WIN_SIZE_PROPORTION; j++)
-			set_img_pixel(&context->img, x + j, y + i, 0xFFFFFF);
+	if (context->inputs_handler.inputs[6]) // Shift_L
+		context->minimap.mm_zoom = 14;
+	for (int i = 0; i < context->minimap.mm_zoom * WIN_SIZE_PROPORTION; i++)
+		for (int j = 0; j < context->minimap.mm_zoom * WIN_SIZE_PROPORTION; j++)
+			if (i == 0 || j == 0)
+				set_img_pixel(&context->img, x + j, y + i, 0x0);	//	Use other color ?
+			else
+				set_img_pixel(&context->img, x + j, y + i, 0xFFFFFF);	//	Use other color ?
 }
 
-static void	search_walls(t_context *context)
+static void	render_walls(t_context *context)
 {
+	if (context->inputs_handler.inputs[6]) // Shift_L
+	{
+		context->minimap.mm_zoom = 14;
+		context->minimap.max_search = 15;
+	}
 	// search walls surrounding player
-	for (int i = -MAX_SEARCH; i <= MAX_SEARCH; i++)
-		for (int j = -MAX_SEARCH; j <= MAX_SEARCH; j++)
+	for (int i = -context->minimap.max_search; i <= context->minimap.max_search; i++)
+		for (int j = -context->minimap.max_search; j <= context->minimap.max_search; j++)
 			if (get_map_char(&(context->map), (int)context->map.player.pos.x + j, (int)context->map.player.pos.y + i) == '1')
-				draw_square(context, (7 * (context->win_width / 8)) + ((j + (int)context->map.player.pos.x - context->map.player.pos.x) * MM_ZOOM * WIN_SIZE_PROPORTION), context->win_height - ((context->win_width / 8)) + ((i + (int)context->map.player.pos.y - context->map.player.pos.y) * MM_ZOOM * WIN_SIZE_PROPORTION)); // Magic number
+				draw_square(context, (7 * (context->win_width / 8)) + ((j + (int)context->map.player.pos.x - context->map.player.pos.x) * context->minimap.mm_zoom * WIN_SIZE_PROPORTION), context->win_height - ((context->win_width / 8)) + ((i + (int)context->map.player.pos.y - context->map.player.pos.y) * context->minimap.mm_zoom * WIN_SIZE_PROPORTION)); // Magic number
 }
 
 void	render_minimap(t_context *context)
 {
+	context->minimap.max_search = MAX_SEARCH;
+	context->minimap.mm_zoom = MM_ZOOM;
 	//	calc minimap center position on screen.	//	Save in variable!
 	set_img_pixel(&context->img, (7 * (context->win_width / 8))/* - MARGIN*/, context->win_height - ((context->win_width / 8))/* - MARGIN*/, 0XFFFFFF);	//	Test pixel
 	//	render walls
-		search_walls(context);
+		render_walls(context);
 	//	render rays
 		//for (int i = 0; i < 3; i++)
 			//	draw minimap ray (with length depending on character speed / running)	//	need to import and adapt my line drawing function from fdf.
